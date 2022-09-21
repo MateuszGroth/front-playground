967da314d5f046663bcbe01b8151cfd5bb74c720
MateuszGroth <mateusz.groth1@gmail.com>
2022-09-21 13:51:31 +0200

    craco setup fix

---
+++ react-form/src/App.tsx
@@ -0,0 +1,16 @@
+import { Container, Paper, Grid } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+    <Container>
+      <Paper elevation={2}>
+        <Grid container spacing={2}>
+          Hello
+        </Grid>
+      </Paper>
+    </Container>
+  )
+}
+
+export default App
