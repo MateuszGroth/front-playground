067833231d2f440a0d90a96b8d1f39e650a4b5cd
MateuszGroth <mateusz.groth1@gmail.com>
2022-10-07 13:36:09 +0200

    wasm

--- cra-assembly/src/wasm/wasm.js
+++ cra-assembly/src/wasm/wasm.js
@@ -1,21 +1,21 @@
 import loader from "@assemblyscript/loader";
 
-const imports= {};
+const imports = {};
 
 const getWasmInstance = (() => {
-    let wasmInstance;
-    return () => {
-        if (wasmInstance) {
-            return wasmInstance;
-        }
+  let wasmInstance;
+  return () => {
+    if (wasmInstance) {
+      return wasmInstance;
+    }
 
-        return wasmInstance = loader
-        .instantiate(fetch(new URL("webassembly.wasm", import.meta.url)), imports)
-        .then(({ exports }) => exports);
-    };
+    return (wasmInstance = loader
+      .instantiate(fetch(new URL("webassembly.wasm", import.meta.url)), imports)
+      .then(({ exports }) => exports));
+  };
 })();
 
-export const add = (...arg) => getWasmInstance().then(wasmInstance => wasmInstance.add(...arg));
+export const add = (...arg) =>
+  getWasmInstance().then((wasmInstance) => wasmInstance.add(...arg));
 
 export default getWasmInstance;
-
