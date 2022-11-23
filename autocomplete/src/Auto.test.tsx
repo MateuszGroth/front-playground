2e607633609c0461c250a4b1f6a08c120f650f9b
MateuszGroth <mateusz.groth1@gmail.com>
2022-11-23 11:44:03 +0100

    some tests stuff

--- autocomplete/src/Auto.test.tsx
+++ autocomplete/src/Auto.test.tsx
@@ -1,18 +1,53 @@
 /* eslint-disable camelcase */
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
 import Auto from './Auto'
 
+jest.useFakeTimers()
+
+const setup = () => {
+  fetchMock.mockResponse((req) => {
+    if (req.url.includes('pokemon')) {
+      return new Promise((resolve) => {
+        resolve(
+          JSON.stringify({
+            count: 100,
+            next: 'pokemons10',
+            previous: null,
+            results: [{ name: 'Bulbasaur', url: 'pokemon/bulbazaur' }],
+          })
+        )
+      })
+    }
+
+    return Promise.reject(new Error('bad url'))
+  })
+  // const user = userEvent.setup()
+  const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime })
+  // jest.advanceTimersByTime(1000) when want to something in setTimeout to be executed
+  const utils = render(
+    <QueryClientProvider client={new QueryClient()}>
+      <Auto />
+    </QueryClientProvider>
+  )
+
+  return { user, ...utils }
+}
 describe('Auto', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    fetchMock.resetMocks()
+  })
   it('should render correctly', () => {
-    render(<Auto />)
+    setup()
     screen.getByTestId('autocomplete')
   })
 
   it('should show searched options', async () => {
-    render(<Auto />)
-    userEvent.type(screen.getByTestId('autocomplete'), 'bulba')
+    const { user } = setup()
+    await user.type(screen.getByTestId('autocomplete'), 'bulba')
     await screen.findByText('Loading…')
     await screen.findByText('Bulbasaur')
   })
@@ -23,15 +58,17 @@ describe('Auto', () => {
       .mockImplementation(
         () => new Promise((resolve) => resolve({ json: () => Promise.resolve({ pokemons: [] }) } as any))
       )
-    render(<Auto />)
-    userEvent.type(screen.getByTestId('location_select'), 'bu')
-    userEvent.type(screen.getByTestId('location_select'), 'lb')
-    userEvent.type(screen.getByTestId('location_select'), 'a')
-
+    const { user } = setup()
+    expect(fetchSpy).toHaveBeenCalledTimes(0)
+    await user.type(screen.getByTestId('autocomplete'), 'bu')
+    expect(fetchSpy).toHaveBeenCalledTimes(1) // fetch on focus with empty query
+    await user.type(screen.getByTestId('autocomplete'), 'lb')
+    await user.type(screen.getByTestId('autocomplete'), 'a')
+    expect(fetchSpy).toHaveBeenCalledTimes(1) // wait for delay to pass
     await screen.findByText('Loading…')
     await waitFor(() => expect(screen.queryByText('Loading…')).toBeNull())
 
-    expect(fetchSpy).toHaveBeenCalledTimes(1)
+    expect(fetchSpy).toHaveBeenCalledTimes(2)
     fetchSpy.mockRestore()
   })
 })
