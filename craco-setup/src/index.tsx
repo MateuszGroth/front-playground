e0234fd71799829a7ece59b1d072450b4e60b69e
MateuszGroth <mateusz.groth1@gmail.com>
2022-12-02 13:24:27 +0100

    sw craco

--- craco-setup/src/index.tsx
+++ craco-setup/src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App'
+import { onServiceWorkerUpdate } from './utils/onServiceWorkerUpdate'
 import reportWebVitals from './reportWebVitals'
+import * as serviceWorker from './serviceWorker'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -15,3 +17,12 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+if (process.env.NODE_ENV !== 'development') {
+  const config = {
+    onUpdate: onServiceWorkerUpdate,
+  }
+  serviceWorker.register(config)
+} else {
+  serviceWorker.unregister()
+}
