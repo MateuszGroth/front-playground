2af118dc2b069a7088ac8b88c4d50d39348545ab
MateuszGroth <39166248+MateuszGroth@users.noreply.github.com>
2023-02-13 12:18:29 +0100

    export

--- exports/src/export/xlsx/xlsx.ts
+++ exports/src/export/xlsx/xlsx.ts
@@ -1,42 +1,61 @@
-import { utils, writeFile } from 'xlsx'
+import { utils, writeFile, WorkSheet } from 'xlsx'
 
-export interface XLSXExportArg<T> {
+type Column<T extends Record<string, unknown>> = { key: keyof T; label?: string }
+export interface XLSXExportArg<T extends Record<string, unknown>> {
   data: Array<T>
-  orderedKeys: (keyof T)[]
-  headerMap?: Record<keyof T, string>
+  columns: Column<T>[]
   fileName?: string
 }
 
-const getColumnsWidths = <T>(
-  data: T[],
-  orderedKeys: (keyof T)[],
-  headerMap?: Record<keyof T, string>
-): Array<{ wch: number }> => {
-  const initialWidths = orderedKeys.map((key) => headerMap?.[key].length ?? String(key).length)
-
-  return data
-    .reduce((acc: number[], row: T) => {
-      orderedKeys.forEach((key, i) => {
-        if (!acc[i]) {
-          acc[i] = 10
-        }
-        acc[i] = Math.max(acc[i], String(row[key])?.length ?? 0)
-      })
-      return acc
-    }, initialWidths)
-    .map((wch: number) => ({ wch }))
-}
-
-export const exportToXLSX = <T>({ data, orderedKeys, headerMap, fileName }: XLSXExportArg<T>): void => {
-  const worksheet = utils.json_to_sheet(JSON.parse(JSON.stringify(data)), {
+export const exportToXLSX = <T extends Record<string, unknown>>({
+  data,
+  columns,
+  fileName,
+}: XLSXExportArg<T>): void => {
+  const orderedKeys = getOrderedKeys(columns)
+  const worksheet = utils.json_to_sheet(cloneDeep(data), {
     header: orderedKeys as string[], // use correct order of keys
   })
 
-  worksheet['!cols'] = getColumnsWidths(data, orderedKeys, headerMap)
-  utils.sheet_add_aoa(worksheet, [orderedKeys.map((key) => headerMap?.[key] ?? key)], { origin: 'A1' })
+  setColumnsWidths(worksheet, data, columns)
+  addHeaderRow(worksheet, columns)
 
   const workbook = utils.book_new()
-  utils.book_append_sheet(workbook, worksheet, 'Report')
 
   writeFile(workbook, (fileName ?? 'data') + '.xlsx')
+}
+
+const cloneDeep = (data: any) => JSON.parse(JSON.stringify(data))
+
+const getOrderedKeys = <T extends Record<string, unknown>>(columns: Column<T>[]): (keyof T)[] => {
+  return columns.map(({ key }) => key)
+}
+
+const addHeaderRow = <T extends Record<string, unknown>>(worksheet: WorkSheet, columns: Column<T>[]): void => {
+  utils.sheet_add_aoa(worksheet, [columns.map(({ key, label }) => label ?? key)], { origin: 'A1' })
+}
+
+const setColumnsWidths = <T extends Record<string, unknown>>(
+  worksheet: WorkSheet,
+  data: T[],
+  columns: Column<T>[]
+): void => {
+  const MAX_CELL_WIDTH = 80
+  const initialWidths = columns.map(({ key, label }) => String(label).length ?? String(key).length)
+
+  const columnsWidths = data
+    .reduce((acc: number[], row: T) => {
+      columns.forEach(({ key }, i) => {
+        if (!acc[i]) {
+          acc[i] = 10
+        }
+        const cellContentLength = String(row[key])?.length ?? 0
+        acc[i] = Math.max(acc[i], cellContentLength)
+      })
+
+      return acc
+    }, initialWidths)
+    .map((wch: number) => ({ wch: wch > MAX_CELL_WIDTH ? MAX_CELL_WIDTH : wch }))
+
+  worksheet['!cols'] = columnsWidths
 }
