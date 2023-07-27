7b0b8f32aa675dd54fb78de1a31fb5c2832dacc8
Mateusz Groth <mateusz.groth1@gmail.com>
2023-07-27 15:14:31 +0200

    int observer hook

---
+++ my-hooks/src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,29 @@
+import { RefObject, useEffect, useState } from 'react'
+
+const useIntersectionObserver = <TRef extends RefObject<HTMLElement>>(
+  ref: TRef,
+  options?: IntersectionObserverInit
+) => {
+  const [isIntersecting, setIsIntersecting] = useState(false)
+
+  useEffect(() => {
+    const observedElement = ref.current
+    if (!observedElement) {
+      return
+    }
+
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting)
+    }, options)
+
+    observer.observe(observedElement)
+
+    return () => {
+      if (observedElement) {
+        observer.unobserve(observedElement)
+      }
+    }
+  }, [])
+
+  return isIntersecting
+}
