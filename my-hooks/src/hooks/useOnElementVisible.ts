7e014506531099e61b6e014b74aa6f82470c4b5e
Mateusz Groth <mateusz.groth1@gmail.com>
2023-07-27 15:48:10 +0200

    improve

--- my-hooks/src/hooks/useOnElementVisible.ts
+++ my-hooks/src/hooks/useOnElementVisible.ts
@@ -1,23 +1,31 @@
-import { RefObject, useEffect } from 'react'
+import type { RefObject } from 'react'
+import { useMemo } from 'react'
+import { useEffect } from 'react'
+import useCallbackRef from './useCallbackRef'
 
+//trigger callback if element is visible in the viewport
 export const useOnElementVisible = (
   target: RefObject<HTMLElement>,
   callback: (isVisible: boolean) => void,
   options?: IntersectionObserverInit
 ) => {
+  const { root, rootMargin, threshold } = options ?? {}
+  const callbackRef = useCallbackRef(callback)
+
+  const memoizedOptions = useMemo(() => {
+    return { root, rootMargin, threshold }
+  }, [root, rootMargin, threshold])
+
   useEffect(() => {
-    if (!target.current) {
-      return
+    if (target?.current) {
+      const observer = new IntersectionObserver((entries) => {
+        const { isIntersecting } = entries[0]
+        callbackRef.current(isIntersecting)
+      }, memoizedOptions)
+      observer.observe(target.current)
+      return () => {
+        observer.disconnect()
+      }
     }
-
-    const observer = new IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0]
-      callback(isIntersecting)
-    }, options)
-    observer.observe(target.current)
-
-    return () => {
-      observer.disconnect()
-    }
-  }, [target, callback, options])
+  }, [target, callbackRef, memoizedOptions])
 }
